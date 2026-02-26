9efdb0eb77e8563abdc905cf2e9ce1877702dd78
JustTOE <tintareanumihai@gmail.com>
2026-02-26 20:19:00 +0200

    Docker setup

---
+++ src/ulms/src/main/java/dev/tmmc/ulms/UlmsApplication.java
@@ -0,0 +1,21 @@
+package dev.tmmc.ulms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class UlmsApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(UlmsApplication.class, args);
+	}
+
+}
---
+++ src/ulms/src/test/java/dev/tmmc/ulms/UlmsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tmmc.ulms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
